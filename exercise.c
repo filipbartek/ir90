6456ff683ec4209b83a6c5677376b33aca93d8b6
Filip Bártek <filip.bartek@hotmail.com>
2013-05-24 18:49:03 +0200

    This implementation is correct and worth 4 points.
    Result message:
    
    exercise.c:227:1: warning: ‘rotate_part_large’ defined but not used [-Wunused-function]
    
    top - 16:59:52 up 59 days, 20:12,  0 users,  load average: 0.40, 0.10, 0.07
    Tasks: 106 total,   1 running, 105 sleeping,   0 stopped,   0 zombie
    %Cpu(s):  0.0 us,  0.0 sy,  0.0 ni,100.0 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
    KiB Mem:  16363680 total, 16143128 used,   220552 free,   139552 buffers
    KiB Swap:        0 total,        0 used,        0 free, 15457296 cached
    
    >>> Will compute time needed for 20 iterations averaged over 4 runs.
    >>> The measured mean time, its probable lower and upper bounds and S.D.
    >>> are printed out. See the header of statistics.awk for details.
    =1.603
    =1.639
    =1.629
    =1.647
    1.6295 1.61036 1.64864 0.0191398 6.518 4
    ** RESULT: ** Performance 1.6295, resulting in 4 points.

--- exercise.c
+++ exercise.c
@@ -1,18 +1,316 @@
 #include <stdio.h>
+#include <assert.h>
+// assert
+#include <inttypes.h>
+// uint32_t
+#include <string.h>
+// memset
+//#include <x86intrin.h>
+//#include <emmintrin.h>
+#include <limits.h>
+// CHAR_BIT
+#include <endian.h>
 #include "aim.h"
+
+//#define VARIANT_SIMPLE
+// 1 test1024.pbm 0.011
+#define VARIANT_DNC // Divide aNd Conquer
+// 1 test16384.pbm
+// OMP
+// 1.180
+// 1.051
+// 1.004
+// 0.691
+// 0.489
+// 0.400
+// 0.339
+// 0.148
+
+#define VARIANT_OMP // parallel
+//#define VARIANT_32
+#define VARIANT_ACCESS
+
+//#define AREA_SMALL_DIM 4096
+// up: too much
+// down: small enough
+#define AREA_SMALL_DIM (2048)
+//#define AREA_SMALL_DIM 1024
+//#define AREA_SMALL_DIM 512
+// 2.351
+//#define AREA_SMALL_DIM 256
+// 2.318 2.300
+//#define AREA_SMALL_DIM 128
+// 2.338
+//#define AREA_SMALL_DIM 64
+// 2.358
+//#define AREA_SMALL_DIM 32
+// 2.477
+#define AREA_SMALL (AREA_SMALL_DIM * AREA_SMALL_DIM)
+
+#ifdef VARIANT_ACCESS
+
+// User defined:
+//typedef uint32_t AccessWordType;
+//#define ACCESS_WORD_BITS (8)
+//#define ACCESS_WORD_BITS (32)
+#define ACCESS_WORD_BITS (64)
+// ACCESS_WORD_BITS must be equal to (sizeof(AccessWordType) / CHAR_BIT)
+//#define ACCESS_BYTES (2)
+#define ACCESS_BYTES (8)
+//#define ACCESS_BYTES (16)
+// ACCESS_BYTES must be a multiple of sizeof(AccessWordType)
+
+// Automatic:
+#define BYTE_BITS (CHAR_BIT)
+//assert(ACCESS_WORD_BITS % BYTE_BITS == 0);
+#define ACCESS_WORD_BYTES (ACCESS_WORD_BITS / BYTE_BITS)
+
+// Define access_htobe and access_betoh
+#if (ACCESS_WORD_BITS == 8)
+typedef uint8_t AccessWordType;
+#define access_htobe
+#define access_betoh
+#elif (ACCESS_WORD_BITS == 16)
+typedef uint16_t AccessWordType;
+#define access_htobe htobe16
+#define access_betoh be16toh
+#elif (ACCESS_WORD_BITS == 32)
+typedef uint32_t AccessWordType;
+#define access_htobe htobe32
+#define access_betoh be32toh
+#elif (ACCESS_WORD_BITS == 64)
+typedef uint64_t AccessWordType;
+#define access_htobe htobe64
+#define access_betoh be64toh
+#else
+#error Unsupported ACCESS_WORD_BITS; must be 8, 16, 32 or 64.
+#endif
+
+#if (ACCESS_WORD_BYTES == 1)
+#define access_htobe
+#define access_betoh
+#elif (ACCESS_WORD_BYTES == 2)
+#define access_htobe htobe16
+#define access_betoh be16toh
+#elif (ACCESS_WORD_BYTES == 4)
+#define access_htobe htobe32
+#define access_betoh be32toh
+#elif (ACCESS_WORD_BYTES == 8)
+#define access_htobe htobe64
+#define access_betoh be64toh
+#else
+#error Unsupported ACCESS_WORD_BYTES; must be 2, 4 or 8.
+#endif
+
+//#if (ACCESS_WORD_BYTES != ACCESS_BYTES)
+typedef __attribute__((aligned(ACCESS_BYTES), vector_size(ACCESS_BYTES)))
+AccessWordType AccessType;
+//assert(ACCESS_BYTES % ACCESS_WORD_BYTES == 0);
+#define ACCESS_WORDS (ACCESS_BYTES / ACCESS_WORD_BYTES)
+//#else
+//typedef __attribute__((aligned(ACCESS_BYTES))) AccessWordType AccessType;
+//#define ACCESS_WORDS (1)
+//#endif
+
+#define ACCESS_BITS (ACCESS_BYTES * BYTE_BITS)
+#define ACCESS_AREA_SMALL_DIM (AREA_SMALL_DIM / ACCESS_BITS)
+#define ACCESS_AREA_SMALL (ACCESS_AREA_SMALL_DIM * ACCESS_AREA_SMALL_DIM)
+
+#if (ACCESS_WORDS == 1)
+static const AccessType ACCESS_ONE = {1};
+#elif (ACCESS_WORDS == 2)
+static const AccessType ACCESS_ONE = {1, 1};
+#elif (ACCESS_WORDS == 4)
+static const AccessType ACCESS_ONE = {1, 1, 1, 1};
+#else
+#error Unsupported ACCESS_WORDS; must be 1, 2 or 4.
+#endif
+
+static inline __attribute__((hot))
+AccessWordType access_word_decode(const AccessWordType word)
+{
+return access_htobe(word);
+}
+
+static inline __attribute__((hot))
+AccessWordType access_word_encode(const AccessWordType word)
+{
+return access_betoh(word);
+}
+
+static inline __attribute__((hot))
+AccessType decode(const AccessType n)
+{
+AccessType result;
+for (unsigned int i = 0; i < ACCESS_WORDS; i++) {
+result[i] = access_word_decode(n[i]);
+}
+return result;
+}
+
+static inline __attribute__((hot))
+AccessType encode(const AccessType n)
+{
+AccessType result;
+for (unsigned int i = 0; i < ACCESS_WORDS; i++) {
+result[i] = access_word_encode(n[i]);
+}
+return result;
+}
+
+static inline __attribute__((hot))
+void rotate_part_atomic(
+const AccessType * const restrict in, AccessType * const restrict out,
+const unsigned int rowwidth,
+const unsigned int in_x, const unsigned int in_y,
+const unsigned int out_x, const unsigned int out_y)
+{
+AccessType get_rows[ACCESS_BITS];
+AccessType put_rows[ACCESS_BITS];
+memset(put_rows, 0, ACCESS_BITS * ACCESS_BYTES);
+
+for (unsigned int y = 0; y < ACCESS_BITS; y++) {
+get_rows[y] = decode(in[(in_y * ACCESS_BITS + y) * rowwidth + in_x]);
+}
+for (unsigned int get_y = 0; get_y < ACCESS_BITS; get_y++) {
+unsigned int put_x = ACCESS_BITS - 1 - get_y;
+unsigned int put_word = put_x / ACCESS_WORD_BITS;
+unsigned int put_bit = put_x % ACCESS_WORD_BITS;
+for (unsigned int get_x = 0; get_x < ACCESS_BITS; get_x++) {
+unsigned int put_y = get_x;
+unsigned int get_word = get_x / ACCESS_WORD_BITS;
+unsigned int get_bit = get_x % ACCESS_WORD_BITS;
+AccessWordType get_data = (get_rows[get_y][get_word] >>
+(ACCESS_WORD_BITS - 1 - get_bit)) & 1;
+AccessWordType put_data = get_data <<
+(ACCESS_WORD_BITS - 1 - put_bit);
+put_rows[put_y][put_word] = put_rows[put_y][put_word] | put_data;
+}
+}
+for (unsigned int put_y = 0; put_y < ACCESS_BITS; put_y++) {
+out[(out_y * ACCESS_BITS + put_y) * rowwidth + out_x] =
+encode(put_rows[put_y]);
+/*for (unsigned int x = 0; x < ACCESS_BITS; x++) {
+unsigned int p_word = x / ACCESS_WORD_BITS;
+unsigned int p_bit = ACCESS_WORD_BITS - 1 - (x % ACCESS_WORD_BITS);
+bool p = (out_rows[y][p_word] >> (p_bit)) & 1;
+putchar(".#"[p]);
+}
+putchar('\n');*/
+}
+}
+
+static inline void
+rotate_part_small(
+const AccessType * const restrict in, AccessType * const restrict out,
+const unsigned int rowwidth,
+const unsigned int in_x, const unsigned int in_y,
+const unsigned int out_x, const unsigned int out_y,
+const unsigned int width, const unsigned int height)
+{
+#ifdef VARIANT_OMP
+#pragma omp parallel for
+#endif // VARIANT_OMP
+for (unsigned int y = 0; y < height; y++) {
+unsigned int sub_in_y = in_y + y;
+unsigned int sub_out_x = out_x + width - 1 - y;
+for (unsigned int x = 0; x < width; x++) {
+unsigned int sub_in_x = in_x + x;
+unsigned int sub_out_y = out_y + x;
+rotate_part_atomic(in, out, rowwidth, sub_in_x, sub_in_y,
+sub_out_x, sub_out_y);
+}
+}
+}
+
+static void
+rotate_part_large(
+const AccessType * const restrict in, AccessType * const restrict out,
+const unsigned int rowwidth,
+const unsigned int in_x, const unsigned int in_y,
+const unsigned int out_x, const unsigned int out_y,
+const unsigned int width, const unsigned int height)
+{
+// unsigned int arguments' unit is uint32_t
+if (width * height <= ACCESS_AREA_SMALL) { // small
+rotate_part_small(in, out, rowwidth, in_x, in_y, out_x, out_y,
+width, height);
+} else { // large
+assert(width % 2 == 0);
+assert(height % 2 == 0);
+const unsigned int sub_width = width / 2;
+const unsigned int sub_height = height / 2;
+#ifdef VARIANT_OMP
+#pragma omp parallel for
+#endif // VARIANT_OMP
+for (unsigned int quarter = 0; quarter < 4; quarter++) {
+unsigned int sub_in_x = 0;
+unsigned int sub_in_y = 0;
+unsigned int sub_out_x = 0;
+unsigned int sub_out_y = 0;
+switch (quarter) {
+case 0:
+sub_in_x = in_x;
+sub_in_y = in_y;
+sub_out_x = out_x + sub_width;
+sub_out_y = out_y;
+break;
+case 1:
+sub_in_x = in_x + sub_width;
+sub_in_y = in_y;
+sub_out_x = out_x + sub_width;
+sub_out_y = out_y + sub_height;
+break;
+case 2:
+sub_in_x = in_x + sub_width;
+sub_in_y = in_y + sub_height;
+sub_out_x = out_x;
+sub_out_y = out_y + sub_height;
+break;
+case 3:
+sub_in_x = in_x;
+sub_in_y = in_y + sub_height;
+sub_out_x = out_x;
+sub_out_y = out_y;
+break;
+}
+rotate_part_large(in, out, rowwidth, sub_in_x, sub_in_y,
+sub_out_x, sub_out_y, sub_width, sub_height);
+}
+}
+}
+
+#endif // VARIANT_ACCESS
 
 void
 exercise(struct image * restrict in, struct image * restrict out)
 {
-	/* Do your image transformation here. Delete the code below
-	 * first, especially when testing with large images! Modify
-	 * the image, not just the printing code. */
-	for (int y = 0; y < in->rows; y++) {
-		for (int x = 0; x < in->cols; x++) {
-			bool p = image_getpixel(in, x, y);
-			putchar(".#"[p]);
-			image_putpixel(out, x, y, p);
-		}
-		putchar('\n');
-	}
+#ifdef VARIANT_SIMPLE
+for (unsigned int y = 0; y < in->rows; y++) {
+for (unsigned int x = 0; x < in->cols; x++) {
+bool p = image_getpixel(in, x, y);
+image_putpixel(out, in->cols - 1 - y, in->rows - 1 - x, p);
+}
+}
+#endif // VARIANT_SIMPLE
+#ifdef VARIANT_ACCESS
+const AccessType * const in_access = (const AccessType * const)in->bitmap;
+AccessType * const out_access = (AccessType * const)out->bitmap;
+
+assert(image_rowbytes(in) == image_rowbytes(out));
+assert(image_rowbytes(in) % ACCESS_BYTES == 0);
+const unsigned int rowwidth_access = image_rowbytes(in) / ACCESS_BYTES;
+
+assert(in->cols == out->cols);
+assert(in->cols % ACCESS_BITS == 0);
+const unsigned int width_access = in->cols / ACCESS_BITS;
+
+assert(in->rows == out->rows);
+assert(in->rows % ACCESS_BITS == 0);
+const unsigned int height_access = in->rows / ACCESS_BITS;
+
+rotate_part_small(in_access, out_access, rowwidth_access, 0, 0, 0, 0,
+width_access, height_access);
+// TODO Tweak small / large - divide parts between cores efficiently.
+#endif // VARIANT_ACCESS
 }
